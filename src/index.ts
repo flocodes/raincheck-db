dc81cd30fb84f44bec2db52b85255ba0845baaa3
flowrites <flodoesthings@gmail.com>
2020-03-07 18:29:30 +0100

    Use container from docker hub for production

--- src/index.ts
+++ src/index.ts
@@ -36,10 +36,12 @@ server.express.use(cookieParser())
 server.express.use(getCookie)
 
 // TODO: Safe options?
+const playground: false|string = (process.env.NODE_ENV === 'production') ? false : '/'
 const opts = {
   cors: {
     credentials: true,
     origin: ['http://localhost:4000', 'http://localhost:3000']
-  }
+  },
+  playground
 }
 server.start(opts, () => console.log('Server is running on http://localhost:4000'))
